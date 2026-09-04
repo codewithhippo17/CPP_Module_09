92a4acd673b957a92ab110eb81b6186ae85a4a50
_hippo <elhaiba.hamza17@gmail.com>
2026-09-04 02:00:40 +0100

    Complete ex02: Implement Ford-Johnson sort for deque and add final outputs

--- ex02/PmergeMe.cpp
+++ ex02/PmergeMe.cpp
@@ -90,3 +90,229 @@ void PmergeMe::processInput(int argc, char **argv)
   if (_vectorData.size() < 2 || _dequeData.size() < 2)
         throw std::runtime_error("Error: Sequence must contain at least 2 numbers to be sorted.");
 }
+
+
+std::vector<int> PmergeMe::sortVector(std::vector<int> &vec)
+{
+    if (vec.size() < 2)
+        return vec;
+
+    if (vec.size() == 2)
+    {
+        if (vec[0] > vec[1])
+            std::swap(vec[0], vec[1]);
+        return vec;
+    }
+
+    int oddNumber = -1;
+    if (vec.size() % 2 != 0)
+    {
+        oddNumber = vec.back();
+        vec.pop_back();
+    }
+
+    std::vector<int> losers;
+    std::vector<int> winners;
+
+    for (size_t i = 0; i < vec.size(); i += 2)
+    {
+        if (vec[i] > vec[i + 1])
+        {
+            losers.push_back(vec[i + 1]);
+            winners.push_back(vec[i]);
+        }
+        else
+        {
+            losers.push_back(vec[i]);
+            winners.push_back(vec[i + 1]);
+        }
+    }
+
+    std::vector<int> winnersTmp = winners;
+    std::vector<int> sortedA = PmergeMe::sortVector(winnersTmp);
+
+    std::vector<int> sortedB(sortedA.size());
+    std::vector<bool> used(winners.size(), false);
+
+    for (size_t i = 0; i < sortedA.size(); i++)
+    {
+        for (size_t j = 0; j < sortedA.size(); j++)
+        {
+            if (winners[j] == sortedA[i] && !used[j])
+            {
+                sortedB[i] = losers[j];
+                used[j] = true;
+                break;
+            }
+        }
+    }
+
+    std::vector<int> res;
+    res.push_back(sortedB[0]);
+
+    std::vector<int> rest(sortedB.size() - 1);
+
+    for (size_t i = 1; i < sortedB.size(); i++)
+        rest[i - 1] = sortedB[i];
+
+    for (size_t i = 0; i < sortedA.size(); i++)
+        res.push_back(sortedA[i]);
+
+    if (oddNumber != -1)
+        rest.push_back(oddNumber);
+
+    std::vector<int> order = generateJacobsthal(rest.size());
+
+    for (size_t i = 0; i < order.size(); i++)
+    {
+        size_t idx = order[i] - 1;
+        int target = rest[idx];
+
+        std::vector<int>::iterator bound;
+
+        int dec = oddNumber == -1 ? 0 : 1;
+        if (idx < rest.size() - dec)
+        {
+            int pairedWinner = sortedA[idx + 1];
+            bound = std::find(res.begin(), res.end(), pairedWinner);
+        }
+        else
+            bound = res.end();
+
+        std::vector<int>::iterator pos = std::lower_bound(res.begin(), bound, target);
+
+        res.insert(pos, target);
+    }
+
+    vec = res;
+    return vec;
+}
+
+std::deque<int> PmergeMe::sortDeque(std::deque<int> &deq)
+{
+    if (deq.size() < 2)
+        return deq;
+
+    if (deq.size() == 2)
+    {
+        if (deq[0] > deq[1])
+            std::swap(deq[0], deq[1]);
+        return deq;
+    }
+
+    int oddNumber = -1;
+    if (deq.size() % 2 != 0)
+    {
+        oddNumber = deq.back();
+        deq.pop_back();
+    }
+
+    std::deque<int> losers;
+    std::deque<int> winners;
+
+    for (size_t i = 0; i < deq.size(); i += 2)
+    {
+        if (deq[i] > deq[i + 1])
+        {
+            losers.push_back(deq[i + 1]);
+            winners.push_back(deq[i]);
+        }
+        else
+        {
+            losers.push_back(deq[i]);
+            winners.push_back(deq[i + 1]);
+        }
+    }
+
+    std::deque<int> winnersTmp = winners;
+    std::deque<int> sortedA = PmergeMe::sortDeque(winnersTmp);
+
+    std::deque<int> sortedB(sortedA.size());
+    std::vector<bool> used(winners.size(), false);
+
+    for (size_t i = 0; i < sortedA.size(); i++)
+    {
+        for (size_t j = 0; j < sortedA.size(); j++)
+        {
+            if (winners[j] == sortedA[i] && !used[j])
+            {
+                sortedB[i] = losers[j];
+                used[j] = true;
+                break;
+            }
+        }
+    }
+
+    std::deque<int> res;
+    res.push_back(sortedB[0]);
+
+    std::deque<int> rest(sortedB.size() - 1);
+
+    for (size_t i = 1; i < sortedB.size(); i++)
+        rest[i - 1] = sortedB[i];
+
+    for (size_t i = 0; i < sortedA.size(); i++)
+        res.push_back(sortedA[i]);
+
+    if (oddNumber != -1)
+        rest.push_back(oddNumber);
+
+    std::vector<int> order = generateJacobsthal(rest.size());
+    for (size_t i = 0; i < order.size(); i++)
+    {
+        size_t idx = order[i] - 1;
+        int target = rest[idx];
+
+        std::deque<int>::iterator bound;
+
+        int dec = oddNumber == -1 ? 0 : 1;
+        if (idx < rest.size() - dec)
+        {
+            int pairedWinner = sortedA[idx + 1];
+            bound = std::find(res.begin(), res.end(), pairedWinner);
+        }
+        else
+            bound = res.end();
+
+        std::deque<int>::iterator pos = std::lower_bound(res.begin(), bound, target);
+
+        res.insert(pos, target);
+    }
+    deq = res;
+    return deq;
+}
+
+void PmergeMe::sortData()
+{
+    struct timeval start, end;
+
+    gettimeofday(&start, NULL);
+    sortVector(_vectorData);
+    gettimeofday(&end, NULL);
+    _vectorTime = (end.tv_sec - start.tv_sec) * 1000000.0 + (end.tv_usec - start.tv_usec);
+
+    gettimeofday(&start, NULL);
+    sortDeque(_dequeData);
+    gettimeofday(&end, NULL);
+    _dequeTime = (end.tv_sec - start.tv_sec) * 1000000.0 + (end.tv_usec - start.tv_usec);
+}
+
+void PmergeMe::printResults(char **argv, int argc) const
+{
+    std::cout << "Before: ";
+    for (int i = 1; i < argc; ++i)
+        std::cout << argv[i] << " ";
+    std::cout << std::endl;
+
+    std::cout << "After:  ";
+    for (size_t i = 0; i < _vectorData.size(); ++i)
+        std::cout << _vectorData[i] << " ";
+    std::cout << std::endl;
+
+    std::cout << "Time to process a range of " << _vectorData.size()
+              << " elements with std::vector : " << _vectorTime << " us" << std::endl;
+
+    std::cout << "Time to process a range of " << _dequeData.size()
+              << " elements with std::deque  : " << _dequeTime << " us" << std::endl;
+}
+
